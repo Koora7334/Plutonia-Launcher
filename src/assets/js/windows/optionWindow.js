6b02bbf467f808383479d3bc5d06a46396d9018d
Pierre-Olivier GOIN <pierreoliviergoin@gmail.com>
2024-11-01 00:05:09 +0100

    rework settings button

--- src/assets/js/windows/optionWindow.js
+++ src/assets/js/windows/optionWindow.js
@@ -1,18 +1,25 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
+const os = require("os");
 
+let isDev = process.env.NODE_ENV === 'dev';
 let optionsWindow;
+
 function createOptionsWindow() {
+    const iconExtension = os.platform() === "win32" ? "ico" : "png";
+
     optionsWindow = new BrowserWindow({
-        width: 300,
-        height: 250,
+        title: "Plutonia - Options",
+        width: 275,
+        height: 225,
+        icon: "./src/assets/images/icon." + iconExtension, // Never change this, its completly fucked up.
         show: false,
         resizable: false,
         modal: true,
         webPreferences: {
             nodeIntegration: true,
-            contextIsolation: false,
-        },
+            contextIsolation: false
+        }
     });
 
     optionsWindow.loadFile(path.join(`${app.getAppPath()}/src/optionPanel.html`));
@@ -20,6 +27,14 @@ function createOptionsWindow() {
     optionsWindow.on('close', (event) => {
         event.preventDefault();
         optionsWindow.hide();
+    });
+
+    optionsWindow.once("ready-to-show", () => {
+        if (isDev) {
+            optionsWindow.webContents.openDevTools({ mode: 'detach' });
+        }
+
+        //updateWindow.show();
     });
 
     optionsWindow.webContents.once('did-finish-load', () => {
@@ -36,18 +51,12 @@ app.on('ready', () => {
 });
 
 ipcMain.on('open-options-panel', (event) => {
-    console.log("Open");
-
     if (optionsWindow) {
         optionsWindow.show();
-    } else {
-        console.error("Options window has not been created.");
     }
 });
 
-ipcMain.once('options-confirm', (event, data) => {
-    console.log("Selected Options:", data);
-
+ipcMain.on('options-confirm', (event, data) => {
     if (optionsWindow) {
         optionsWindow.hide();
     }
