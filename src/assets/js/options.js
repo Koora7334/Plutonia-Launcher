ebeec532ecb6ce523d24ce46ae7b0b4a958d605f
Pierre-Olivier GOIN <pierreoliviergoin@gmail.com>
2024-11-01 22:18:15 +0100

    fix bad config path

--- src/assets/js/options.js
+++ src/assets/js/options.js
@@ -3,7 +3,16 @@ const { ipcRenderer, ipcMain, app } = require('electron');
 const fs = require('fs');
 const path = require('path');
 
-const saveFilePath = path.join(__dirname, '..', 'options.json');
+async function getSaveFilePath() {
+    try {
+        const dataPath = await ipcRenderer.invoke('path-user-data');
+        const saveFilePath = path.join(dataPath, 'options.json');
+
+        return saveFilePath;
+    } catch (error) {
+        console.error('Erreur lors de la récupération du chemin :', error);
+    }
+}
 
 const confirmButton = document.querySelector('.confirm-button');
 
@@ -12,6 +21,7 @@ const ramSelector = document.querySelector('#ram');
 const tabbyChat = document.querySelector('#tc');
 const simpleVoiceChat = document.querySelector('#svc');
 
+/* Called when the confirm button is pressed */
 confirmButton.addEventListener('click', async _ => {
     ipcRenderer.send('options-window-confirm');
 
@@ -21,28 +31,33 @@ confirmButton.addEventListener('click', async _ => {
         "simpleVoiceChat": simpleVoiceChat.checked
     };
 
-    fs.writeFile(saveFilePath, JSON.stringify(data, null, 2), (err) => {
-        if (err) {
-            console.error("Error saving options data:", err);
-        } else {
-            console.log("Options saved successfully.");
-        }
+    getSaveFilePath().then(filePath => {
+        fs.writeFile(filePath, JSON.stringify(data, null, 2), (err) => {
+            if (err) {
+                console.error("Error saving options data:", err);
+            } else {
+                console.log("Options saved successfully.");
+            }
+        });
     });
 });
 
+/* Called when the HTML document is loaded */
 window.addEventListener('load', () => {
-    fs.readFile(saveFilePath, 'utf-8', (err, data) => {
-        if (err) {
-            console.error("Error reading options data:", err);
-            return;
-        }
+    getSaveFilePath().then(filePath => {
+        fs.readFile(filePath, 'utf-8', (err, data) => {
+            if (err) {
+                console.error("Error reading options data:", err);
+                return;
+            }
 
-        const optionsData = JSON.parse(data);
+            const optionsData = JSON.parse(data);
 
-        if (optionsData) {
-            ramSelector.value = optionsData.ram;
-            tabbyChat.checked = optionsData.tabbyChat;
-            simpleVoiceChat.checked = optionsData.simpleVoiceChat;
-        }
+            if (optionsData) {
+                ramSelector.value = optionsData.ram;
+                tabbyChat.checked = optionsData.tabbyChat;
+                simpleVoiceChat.checked = optionsData.simpleVoiceChat;
+            }
+        });
     });
 });
